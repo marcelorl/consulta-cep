c4c992f535d037adac1efe492f94b1b03c550cc0
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2019-04-21 23:03:06 -0300

    chore: right implementation of atomic design

--- src/components/pages/App/App.tsx
+++ src/components/pages/App/App.tsx
@@ -1,29 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import FormSearchContainer from '../../molecules/FormSearchContainer'
+import App from '../../templates/App'
 
-const AppWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-family: arial;
-`
+const AppPage = () => <App />
 
-const Title = styled.h1`
-  font-size: 2rem;
-  font-weight: bold;
-  padding: 2rem 0;
-`
-
-const MapContainer = React.lazy(() => import('../../organims/MapContainer'))
-
-const App = () =>
-  <AppWrapper>
-    <Title>Consulta de endereço</Title>
-    <FormSearchContainer />
-    <React.Suspense fallback={<div>Carregando...</div>}>
-      <MapContainer />
-    </React.Suspense>
-  </AppWrapper>
-
-export default App
+export default AppPage
